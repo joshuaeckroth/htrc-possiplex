f36f98afbe7c786fd089954c0410838840cb4409
Joshua Eckroth <joshuaeckroth@gmail.com>
2024-04-19 12:57:32 -0400

    Added begin/end line character constraints

--- main.py
+++ main.py
@@ -10,14 +10,31 @@ with bz2.open('data.json.bz2', 'rt') as f:
     page_data = data['features']['pages'][0]['body']
     rich.print(page_data)
 
+    line_count = page_data['lineCount'] - page_data['emptyLineCount']
+
     uniq_tokens = sorted(page_data['tokenPosCount'].keys())
     uniq_token_count = len(uniq_tokens)
     token_idx_vars = []
+    token_idx_start_vars = []
+    token_idx_end_vars = []
     for idx in range(page_data['tokenCount']):
         token_idx_choices_vars = []
+        token_idx_choices_start_vars = []
+        token_idx_choices_end_vars = []
         for token in uniq_tokens:
-            token_idx_choices_vars.append(model.NewBoolVar(f'token_{idx}_{token}'))
+            token_var = model.NewBoolVar(f'token_{idx}_{token}')
+            token_idx_choices_vars.append(token_var)
+            token_start_var = model.NewBoolVar(f'token_{idx}_{token}_is_start')
+            token_idx_choices_start_vars.append(token_start_var)
+            token_end_var = model.NewBoolVar(f'token_{idx}_{token}_is_end')
+            token_idx_choices_end_vars.append(token_end_var)
+            model.AddImplication(token_var.Not(), token_start_var.Not())
+            model.AddImplication(token_var.Not(), token_end_var.Not())
+            model.AddImplication(token_start_var, token_var)
+            model.AddImplication(token_end_var, token_var)
         token_idx_vars.append(token_idx_choices_vars)
+        token_idx_start_vars.append(token_idx_choices_start_vars)
+        token_idx_end_vars.append(token_idx_choices_end_vars)
         # each position must have exactly one token
         model.Add(sum(token_idx_choices_vars) == 1)
     
@@ -26,6 +43,41 @@ with bz2.open('data.json.bz2', 'rt') as f:
         token_vars = [token_idx_vars[i][idx] for i in range(page_data['tokenCount'])]
         pos_sum = sum(page_data['tokenPosCount'][token].values())
         model.Add(sum(token_vars) == pos_sum)
+
+    # for each token, count how many times it is used as a start
+    # make sure this count equals beginCharCount
+    for begin_char in page_data['beginCharCount']:
+        begin_char_count = page_data['beginCharCount'][begin_char]
+        start_vars = []
+        for token_idx, token in enumerate(uniq_tokens):
+            if token[0] == begin_char:
+                for idx in range(page_data['tokenCount']):
+                    start_vars.append(token_idx_start_vars[idx][token_idx])
+        # need to check that we actually found tokens that start/end with this character,
+        # because we have seen unicode issues in the data
+        # for example: "htid": "penn.ark:/81431/p3k35mf4d"
+        if len(start_vars) > 0:
+            model.Add(sum(start_vars) == begin_char_count)
+
+    for end_char in page_data['endCharCount']:
+        end_char_count = page_data['endCharCount'][end_char]
+        end_vars = []
+        for token_idx, token in enumerate(uniq_tokens):
+            if token[-1] == end_char:
+                for idx in range(page_data['tokenCount']):
+                    end_vars.append(token_idx_end_vars[idx][token_idx])
+        if len(end_vars) > 0:
+            model.Add(sum(end_vars) == end_char_count)
+
+    all_start_vars = []
+    all_end_vars = []
+    for idx in range(page_data['tokenCount']):
+        start_vars = [token_idx_start_vars[idx][token_idx] for token_idx in range(uniq_token_count)]
+        end_vars = [token_idx_end_vars[idx][token_idx] for token_idx in range(uniq_token_count)]
+        all_start_vars.extend(start_vars)
+        all_end_vars.extend(end_vars)
+    model.Add(sum(all_start_vars) == line_count)
+    model.Add(sum(all_end_vars) == line_count)
 
     solver = cp_model.CpSolver()
     solver.parameters.max_time_in_seconds = 10
@@ -37,11 +89,12 @@ with bz2.open('data.json.bz2', 'rt') as f:
             token_vars = [token_idx_vars[i][idx] for i in range(page_data['tokenCount'])]
             print(f'{token}: {sum(solver.Value(v) for v in token_vars)}')
         # print sequence of tokens
-        for tvar_list in token_idx_vars:
-            for idx, tvar in enumerate(tvar_list):
-                if solver.Value(tvar):
-                    print(uniq_tokens[idx], end=' ')
-        print()
+        for idx in range(page_data['tokenCount']):
+            for token_idx in range(uniq_token_count):
+                if solver.Value(token_idx_vars[idx][token_idx]):
+                    if solver.Value(token_idx_start_vars[idx][token_idx]):
+                        print()
+                    print(uniq_tokens[token_idx], end=' ')
     else:
         print('No solution found')
 
